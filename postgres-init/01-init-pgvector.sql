4ea2c77b9609de0ba7f417653817a3d37d028f53
barrax63 <42813483+barrax63@users.noreply.github.com>
2026-01-08 16:04:41 +0100

    Initialize pgvector extension and set PostgreSQL parameters
    This script initializes the pgvector extension and configures PostgreSQL parameters for optimal performance.

---
+++ postgres-init/01-init-pgvector.sql
@@ -0,0 +1,44 @@
+-- Enable pgvector extension for vector operations
+-- This must be run as a superuser (postgres)
+
+-- Create extension in the n8n database
+\c n8n;
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- Verify installation
+SELECT extname, extversion FROM pg_extension WHERE extname = 'vector';
+
+-- PGTune parameters
+-- DB Version: 16
+-- OS Type: linux
+-- DB Type: oltp
+-- Total Memory (RAM): 4 GB
+-- CPUs num: 2
+-- Data Storage: ssd
+
+ALTER SYSTEM SET
+ max_connections = '300';
+ALTER SYSTEM SET
+ shared_buffers = '1GB';
+ALTER SYSTEM SET
+ effective_cache_size = '3GB';
+ALTER SYSTEM SET
+ maintenance_work_mem = '256MB';
+ALTER SYSTEM SET
+ checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET
+ wal_buffers = '16MB';
+ALTER SYSTEM SET
+ default_statistics_target = '100';
+ALTER SYSTEM SET
+ random_page_cost = '1.1';
+ALTER SYSTEM SET
+ effective_io_concurrency = '200';
+ALTER SYSTEM SET
+ work_mem = '3404kB';
+ALTER SYSTEM SET
+ huge_pages = 'off';
+ALTER SYSTEM SET
+ min_wal_size = '2GB';
+ALTER SYSTEM SET
+ max_wal_size = '8GB';
